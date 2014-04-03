9b55602db62a370663b4334d7a08822d4c4b0e3a
Wiggan <wigert@gmail.com>
2014-04-03 21:25:28 +0200

    Better terrain

--- SuperMotorn/Resources/HexaTerrainResource.cpp
+++ SuperMotorn/Resources/HexaTerrainResource.cpp
@@ -26,7 +26,7 @@ HexaTerrainResource::load() {
         geometry->QueryFloatAttribute("slope", &slope);
         geometry->QueryFloatAttribute("distance", &distance);
         mMaterial = geometry->FirstChildElement("material")->GetText();
-        vector<vector<Hexagon> > hexaGrid;
+        vector<vector<Hexagon*> > hexaGrid;
         vector<Vector3> cornerOffsets;
         float sqrt3over2 = sqrt(3) / 2;
         //Outer vertices
@@ -47,14 +47,13 @@ HexaTerrainResource::load() {
         for ( const XMLElement* hexagon = geometry->FirstChildElement("tile"); hexagon != NULL; hexagon = hexagon->NextSiblingElement() ) {
             Vector3 position(hexagon->FirstChildElement("position"));
             float zoffset = (((int)position.getX()) % 2) * (sqrt3over2*radius + distance/2);
-            //float xoffset = (((int)position.getX()) % 2) * ;
             Vector3 p = Vector3(
                 position.getX() * (radius*1.5f + distance*sqrt3over2), 
                 position.getY(), 
                 position.getZ() * (sqrt3over2 * radius*2 + distance) + zoffset);
-            Hexagon h;
-            for ( int i = 0; i < h.positions.size(); ++i ) {
-                h.positions[i] = p + cornerOffsets[i];
+            Hexagon* h = new Hexagon;
+            for ( int i = 0; i < h->positions.size(); ++i ) {
+                h->positions[i] = p + cornerOffsets[i];
             }
             hexaGrid.resize(max(hexaGrid.size(), (int)position.getX()+1));
             hexaGrid[(int)position.getX()].resize(max(hexaGrid[(int)position.getX()].size(), (int)position.getZ()+1));
@@ -62,37 +61,44 @@ HexaTerrainResource::load() {
         }
         for ( int x = 0; x < hexaGrid.size(); x++ ) {
             for ( int z = 0; z < hexaGrid[x].size(); z++ ) {
+                Hexagon* currentHexa = hexaGrid[x][z];
                 if ( z + 1 < hexaGrid[x].size() ) {
-                    hexaGrid[x][z].neighbours[0] = &hexaGrid[x][z + 1];
+                    currentHexa->neighbours[0] = hexaGrid[x][z + 1];
                 }
-                if ( z - 1 > 0 ) {
-                    hexaGrid[x][z].neighbours[3] = &hexaGrid[x][z - 1];
+                if ( z > 0 ) {
+                    currentHexa->neighbours[3] = hexaGrid[x][z - 1];
                 }
                 if ( x % 2 == 1 ) {
                     if ( x + 1 < hexaGrid.size() && z < hexaGrid[x + 1].size() ) {
-                        hexaGrid[x][z].neighbours[1] = &hexaGrid[x + 1][z];
+                        currentHexa->neighbours[2] = hexaGrid[x + 1][z];
                     }
                     if ( x + 1 < hexaGrid.size() && z + 1 < hexaGrid[x + 1].size() ) {
-                        hexaGrid[x][z].neighbours[2] = &hexaGrid[x + 1][z+1];
+                        currentHexa->neighbours[1] = hexaGrid[x + 1][z + 1];
                     }
-                    if ( x - 1 > 0 && z < hexaGrid[x - 1].size() ) {
-                        hexaGrid[x][z].neighbours[4] = &hexaGrid[x - 1][z];
+                    if ( z < hexaGrid[x + 1].size() ) {
+                        currentHexa->neighbours[4] = hexaGrid[x + 1][z];
                     }
-                    if ( x - 1 > 0 && z + 1 < hexaGrid[x - 1].size() ) {
-                        hexaGrid[x][z].neighbours[5] = &hexaGrid[x - 1][z+1];
+                    if ( x > 0 && z + 1 < hexaGrid[x - 1].size() ) {
+                        currentHexa->neighbours[5] = hexaGrid[x - 1][z+1];
                     }
                 } else {
-                    if ( x + 1 < hexaGrid.size() && z > 0 ) {
+                    if ( x + 1 < hexaGrid.size() && z < hexaGrid[x + 1].size() ) {
+                        currentHexa->neighbours[1] = hexaGrid[x + 1][z];
                     }
-                    if ( x + 1 < hexaGrid.size() && z  < hexaGrid[x + 1].size() ) {
+                    if ( x + 1 < hexaGrid.size() && z > 0 && z - 1 < hexaGrid[x + 1].size() ) {
+                        currentHexa->neighbours[2] = hexaGrid[x + 1][z - 1];
                     }
-                    if ( x + 1 < hexaGrid.size() && z > 0 && z < hexaGrid[x + 1].size()) {
+                    if ( x > 0 && z < hexaGrid[x - 1].size() ) {
+                        currentHexa->neighbours[5] = hexaGrid[x - 1][z];
+                    }
+                    if (z > 0 && z - 1 < hexaGrid[x + 1].size() ) {
+                        currentHexa->neighbours[4] = hexaGrid[x + 1][z - 1];
                     }
                 }
-                addFace(hexaGrid[x][z].positions[6], hexaGrid[x][z].positions[10], hexaGrid[x][z].positions[11]);
-                addFace(hexaGrid[x][z].positions[6], hexaGrid[x][z].positions[9], hexaGrid[x][z].positions[10]);
-                addFace(hexaGrid[x][z].positions[6], hexaGrid[x][z].positions[7], hexaGrid[x][z].positions[9]);
-                addFace(hexaGrid[x][z].positions[7], hexaGrid[x][z].positions[8], hexaGrid[x][z].positions[9]);
+                addFace(currentHexa->positions[6], currentHexa->positions[10], currentHexa->positions[11]);
+                addFace(currentHexa->positions[6], currentHexa->positions[9], currentHexa->positions[10]);
+                addFace(currentHexa->positions[6], currentHexa->positions[7], currentHexa->positions[9]);
+                addFace(currentHexa->positions[7], currentHexa->positions[8], currentHexa->positions[9]);
                 vector<Vector3> slopeNormals;
                 slopeNormals.push_back(Vector3(0.0f, 1.0f, 0.0f));
                 slopeNormals.push_back(Vector3(0.0f, 1.0f, 0.0f));
@@ -101,56 +107,51 @@ HexaTerrainResource::load() {
                 slopeNormals.push_back(Vector3(0.0f, 1.0f, 0.0f));
                 slopeNormals.push_back(Vector3(0.0f, 1.0f, 0.0f));
 
-                if ( x % 2 == 1 ) {
-                    if ( x + 1 < hexaGrid.size() && z < hexaGrid[x + 1].size() ) {
-                        addFace(hexaGrid[x][z].positions[1], hexaGrid[x + 1][z].positions[5], hexaGrid[x + 1][z].positions[4]);
-                        addFace(hexaGrid[x][z].positions[1], hexaGrid[x + 1][z].positions[4], hexaGrid[x][z].positions[2]);
-                        //slopeNormals[5] = Vector3(hexaGrid[x][z].positions[2] - hexaGrid[x][z].positions[1]).cross(Vector3(hexaGrid[x][z].positions[2] - hexaGrid[x + 1][z].positions[5])).normalized();
-                    }
-                    if ( x + 1 < hexaGrid.size() && z + 1 < hexaGrid[x + 1].size() ) {
-                        addFace(hexaGrid[x + 1][z + 1].positions[4], hexaGrid[x + 1][z + 1].positions[3], hexaGrid[x][z].positions[0]);
-                        addFace(hexaGrid[x][z].positions[0], hexaGrid[x + 1][z + 1].positions[3], hexaGrid[x][z].positions[1]);
-                    }
-                    if ( x + 1 < hexaGrid.size() && z + 1 < hexaGrid[x + 1].size() && z + 1 < hexaGrid[x].size() ) {
-                        addFace(hexaGrid[x][z].positions[1], hexaGrid[x + 1][z + 1].positions[3], hexaGrid[x + 1][z].positions[5]);
-                        addFace(hexaGrid[x][z+1].positions[2], hexaGrid[x + 1][z + 1].positions[4], hexaGrid[x][z].positions[0]);
-                    }
-                    
-                } else {
-                    if ( x + 1 < hexaGrid.size() && z > 0 ) {
-                        addFace(hexaGrid[x + 1][z - 1].positions[5], hexaGrid[x + 1][z - 1].positions[4], hexaGrid[x][z].positions[2]);
-                        addFace(hexaGrid[x][z].positions[1], hexaGrid[x + 1][z - 1].positions[5], hexaGrid[x][z].positions[2]);
-                        //slopeNormals[2] = Vector3(hexaGrid[x][z].positions[1] - hexaGrid[x][z].positions[2]).cross(Vector3(hexaGrid[x][z].positions[1] - hexaGrid[x + 1][z -1].positions[4])).normalized();
-                    }
-                    if ( x + 1 < hexaGrid.size() && z  < hexaGrid[x + 1].size() ) {
-                        addFace(hexaGrid[x + 1][z].positions[4], hexaGrid[x + 1][z].positions[3], hexaGrid[x][z].positions[1]);
-                        addFace(hexaGrid[x][z].positions[0], hexaGrid[x + 1][z].positions[4], hexaGrid[x][z].positions[1]);
-                    }
-                    if ( x + 1 < hexaGrid.size() && z > 0 && z < hexaGrid[x + 1].size()) {
-                        addFace(hexaGrid[x + 1][z].positions[3], hexaGrid[x + 1][z - 1].positions[5], hexaGrid[x][z].positions[1]);
-                        addFace(hexaGrid[x][z - 1].positions[0], hexaGrid[x][z].positions[2], hexaGrid[x + 1][z - 1].positions[4]);
-                    }
+
+
+
+                if ( currentHexa->neighbours[2] ) {
+                    addFace(currentHexa->neighbours[2]->positions[5], currentHexa->neighbours[2]->positions[4], currentHexa->positions[2]);
+                    addFace(currentHexa->positions[1], currentHexa->neighbours[2]->positions[5], currentHexa->positions[2]);
+                    slopeNormals[2] = Vector3(currentHexa->positions[2] - currentHexa->positions[1]).cross(Vector3(currentHexa->positions[2] - currentHexa->neighbours[2]->positions[4])).normalized();
                 }
-                if ( z + 1 < hexaGrid[x].size()) {
-                    addFace(hexaGrid[x][z].positions[5], hexaGrid[x][z + 1].positions[3], hexaGrid[x][z].positions[0]);
-                    addFace(hexaGrid[x][z].positions[0], hexaGrid[x][z + 1].positions[3], hexaGrid[x][z + 1].positions[2]);
-                    slopeNormals[0] = Vector3(hexaGrid[x][z].positions[5] - hexaGrid[x][z + 1].positions[3]).cross(Vector3(hexaGrid[x][z].positions[5] - hexaGrid[x][z].positions[0])).normalized();
+                if ( currentHexa->neighbours[1] ) {
+                    addFace(currentHexa->neighbours[1]->positions[4], currentHexa->neighbours[1]->positions[3], currentHexa->positions[1]);
+                    addFace(currentHexa->positions[0], currentHexa->neighbours[1]->positions[4], currentHexa->positions[1]);
+                    slopeNormals[1] = Vector3(currentHexa->positions[1] - currentHexa->positions[0]).cross(Vector3(currentHexa->positions[1] - currentHexa->neighbours[1]->positions[4])).normalized();
                 }
-                if ( z - 1 > 0 ) {
-                    slopeNormals[3] = Vector3(hexaGrid[x][z].positions[3] - hexaGrid[x][z].positions[2]).cross(Vector3(hexaGrid[x][z].positions[3] - hexaGrid[x][z -1].positions[0])).normalized();
+                if ( currentHexa->neighbours[1] && currentHexa->neighbours[2] ) {
+                    addFace(currentHexa->neighbours[1]->positions[3], currentHexa->neighbours[2]->positions[5], currentHexa->positions[1]);
                 }
-                addFace(hexaGrid[x][z].positions[5], hexaGrid[x][z].positions[0], hexaGrid[x][z].positions[11], slopeNormals[0]);
-                addFace(hexaGrid[x][z].positions[11], hexaGrid[x][z].positions[0], hexaGrid[x][z].positions[6], slopeNormals[0]);
-                addFace(hexaGrid[x][z].positions[6], hexaGrid[x][z].positions[0], hexaGrid[x][z].positions[1], slopeNormals[1]);
-                addFace(hexaGrid[x][z].positions[1], hexaGrid[x][z].positions[7], hexaGrid[x][z].positions[6], slopeNormals[1]);
-                addFace(hexaGrid[x][z].positions[7], hexaGrid[x][z].positions[1], hexaGrid[x][z].positions[8], slopeNormals[2]);
-                addFace(hexaGrid[x][z].positions[8], hexaGrid[x][z].positions[1], hexaGrid[x][z].positions[2], slopeNormals[2]);
-                addFace(hexaGrid[x][z].positions[9], hexaGrid[x][z].positions[8], hexaGrid[x][z].positions[3], slopeNormals[3]);
-                addFace(hexaGrid[x][z].positions[3], hexaGrid[x][z].positions[8], hexaGrid[x][z].positions[2], slopeNormals[3]);
-                addFace(hexaGrid[x][z].positions[4], hexaGrid[x][z].positions[10], hexaGrid[x][z].positions[9], slopeNormals[4]);
-                addFace(hexaGrid[x][z].positions[9], hexaGrid[x][z].positions[3], hexaGrid[x][z].positions[4], slopeNormals[4]);
-                addFace(hexaGrid[x][z].positions[4], hexaGrid[x][z].positions[5], hexaGrid[x][z].positions[10], slopeNormals[5]);
-                addFace(hexaGrid[x][z].positions[10], hexaGrid[x][z].positions[5], hexaGrid[x][z].positions[11], slopeNormals[5]);
+                if ( currentHexa->neighbours[2] && currentHexa->neighbours[3] ) {
+                    addFace(currentHexa->neighbours[3]->positions[0], currentHexa->positions[2], currentHexa->neighbours[2]->positions[4]);
+                }
+                if ( currentHexa->neighbours[0]) {
+                    addFace(currentHexa->positions[5], currentHexa->neighbours[0]->positions[3], currentHexa->positions[0]);
+                    addFace(currentHexa->positions[0], currentHexa->neighbours[0]->positions[3], currentHexa->neighbours[0]->positions[2]);
+                    slopeNormals[0] = Vector3(currentHexa->positions[5] - currentHexa->neighbours[0]->positions[3]).cross(Vector3(currentHexa->positions[5] - currentHexa->positions[0])).normalized();
+                }
+                if (currentHexa->neighbours[3]) {
+                    slopeNormals[3] = Vector3(currentHexa->positions[3] - currentHexa->positions[2]).cross(Vector3(currentHexa->positions[3] - currentHexa->neighbours[3]->positions[0])).normalized();
+                }
+                if ( currentHexa->neighbours[4] ) {
+                    slopeNormals[4] = Vector3(currentHexa->positions[3] - currentHexa->positions[4]).cross(Vector3(currentHexa->positions[3] - currentHexa->neighbours[4]->positions[1])).normalized();
+                }
+                if ( currentHexa->neighbours[5] ) {
+                    slopeNormals[5] = Vector3(currentHexa->positions[5] - currentHexa->positions[4]).cross(Vector3(currentHexa->positions[5] - currentHexa->neighbours[5]->positions[1])).normalized();
+                }
+                addFace(currentHexa->positions[5], currentHexa->positions[0], currentHexa->positions[11], slopeNormals[0]);
+                addFace(currentHexa->positions[11], currentHexa->positions[0], currentHexa->positions[6], slopeNormals[0]);
+                addFace(currentHexa->positions[6], currentHexa->positions[0], currentHexa->positions[1], slopeNormals[1]);
+                addFace(currentHexa->positions[1], currentHexa->positions[7], currentHexa->positions[6], slopeNormals[1]);
+                addFace(currentHexa->positions[7], currentHexa->positions[1], currentHexa->positions[8], slopeNormals[2]);
+                addFace(currentHexa->positions[8], currentHexa->positions[1], currentHexa->positions[2], slopeNormals[2]);
+                addFace(currentHexa->positions[9], currentHexa->positions[8], currentHexa->positions[3], slopeNormals[3]);
+                addFace(currentHexa->positions[3], currentHexa->positions[8], currentHexa->positions[2], slopeNormals[3]);
+                addFace(currentHexa->positions[4], currentHexa->positions[10], currentHexa->positions[9], slopeNormals[4]);
+                addFace(currentHexa->positions[9], currentHexa->positions[3], currentHexa->positions[4], slopeNormals[4]);
+                addFace(currentHexa->positions[4], currentHexa->positions[5], currentHexa->positions[10], slopeNormals[5]);
+                addFace(currentHexa->positions[10], currentHexa->positions[5], currentHexa->positions[11], slopeNormals[5]);
             }
         }
         
